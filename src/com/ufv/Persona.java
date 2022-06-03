d34726e91b017fdd0ba60012f9a35093fd9be226
Melissa Llajaruna <7903909@alumnos.ufv.es>
2022-06-03 20:57:29 +0200

    First commit

---
+++ src/com/ufv/Persona.java
@@ -0,0 +1,67 @@
+package com.ufv;
+
+public class Persona {
+
+
+
+    private String nombre;
+    private String apellidos;
+    private int dniSinLetra;
+    private int edad;
+
+    public Persona() {
+    }
+    public Persona(String nombre,String apellidos, int dniSinLetra,int edad) {
+        this.nombre = nombre;
+        this.apellidos=apellidos;
+        this.dniSinLetra=dniSinLetra;
+        this.edad=edad;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+
+
+
+
+    public int getDniSinLetra() {
+        return dniSinLetra;
+    }
+
+    public void setDniSinLetra(int dniSinLetra) {
+        this.dniSinLetra = dniSinLetra;
+    }
+
+
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int ed) {
+        if (ed >= 18){
+            System.out.println("Estás hecho un chaval");
+        } else {
+            System.out.println("Ya eres talludito");
+        }
+        this.edad = ed;
+
+    }
+
+
+}
